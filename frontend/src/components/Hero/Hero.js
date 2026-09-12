edf4311da6ea7c608ec18b03e6a6c10ef15628cb
Rahmatov <concorold@gmail.com>
2026-09-12 15:06:06 +0600

    myChangesSH

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import  './Hero.css'
-import hand_icon from '../assests/hand_icon.png'
-import arrow_icon from '../assests/arrow.png'
-import hero_img from '../assests/hero_image.png'
+import React from "react";
+import "./Hero.css";
+import hand_icon from "../assests/hand_icon.png";
+import arrow_icon from "../assests/arrow.png";
+import hero_img from "../assests/hero_image.png";
 const Hero = () => {
-    return (
-        <div className='hero'>
-           <div className='hero-left'>
-              <h2>NEW ARRIVALS ONLY!</h2>
-            <div className='hero-hand-icon'>
-                <p>new</p>
-                <img src={hand_icon}></img>
-            </div>
-                <p>collections</p>
-                <p>for everyone</p>
-            </div> 
-           
-            <div className='hero-latest-btn'>
-                <div>
-                    <div>Latest Collections</div>
-                <img src={arrow_icon}></img>
-                </div>
-            </div>
-
-            <div className='hero-right'>
-                <img src={hero_img} height={700}/>
-            </div> 
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLYyyyyyyyyy!</h2>
+        <div className="hero-hand-icon">
+          <p>new</p>
+          <img src={hand_icon}></img>
         </div>
-    );
+        <p>collections</p>
+        <p>for everyone</p>
+      </div>
+
+      <div className="hero-latest-btn">
+        <div>
+          <div>Latest Collections</div>
+          <img src={arrow_icon}></img>
+        </div>
+      </div>
+
+      <div className="hero-right">
+        <img src={hero_img} height={700} />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- frontend/src/pages/Loginsignup.jsx
+++ frontend/src/pages/Loginsignup.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import '../pages/css/Loginsignup.css'
+import React from "react";
+import "../pages/css/Loginsignup.css";
 
-const  Loginsignup = () => {
-    return (
-      <div className="loginsignup">
-        <div className="loginsignup-container">
-            <h1>Sign Up</h1>
-            <div className="loginsignup-fields">
-                <input type="text" placeholder='Your Name' />
-                <input type="email" placeholder='Email Address' />
-                <input type="password" placeholder='Password' />
-            </div>
-            <button>Continue</button>
-            <p className="loginsignup-login">Already have an account ? <span>Login here</span></p>
-            <div className="loginsignup-agree">
-                
-                <input type="checkbox" name='' id='' />
-                <p>By continuing , i agree to terms & privacy policy.  </p>
-                </div>        
+const Loginsignup = () => {
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign vgcgfcgfcgcgc</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account ? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing , i agree to terms & privacy policy. </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
-export default  Loginsignup;
+export default Loginsignup;
